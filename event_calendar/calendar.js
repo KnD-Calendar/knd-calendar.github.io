4be900749f55dbcfcf867a4e1117ab8a1b8ce414
b-rowan <brett.rowan@icloud.com>
2024-01-06 16:08:58 -0700

    Improve event generator, and add set blitz.

--- event_calendar/calendar.js
+++ event_calendar/calendar.js
@@ -28,38 +28,29 @@ document.addEventListener('DOMContentLoaded', function() {
     calendar.render();
 });
 
-// month calc - (((year*12+month)-1))%5)+1)
-// fire, spirit, earth, air, water
-// 1   , 2     , 3    , 4  , 5
-
 function generateEvents(startDate, endDate) {
     var events = [];
 
-    // Raid: every other friday starting at 19:00 UTC, goes for 51 hours
     var raidEvents = generateRaidDates(startDate, endDate);
     for (const event in raidEvents) {
         events.push(raidEvents[event]);
     }
 
-    // War: every non-raid friday starting at 19:00 UTC, goes for 51 hours
     var warEvents = generateWarDates(startDate, endDate);
     for (const event in warEvents) {
         events.push(warEvents[event]);
     }
 
-    // BRaid: Every monday after a War, starting at 19:00 UTC, goes for 27 hours
     var blitzRaidEvents = generateBlitzRaidDates(startDate, endDate);
     for (const event in blitzRaidEvents) {
         events.push(blitzRaidEvents[event]);
     }
 
-    // BWar: Every monday after a Raid, starting at 19:00 UTC, goes for 27 hours
     var blitzWarEvents = generateBlitzWarDates(startDate, endDate);
     for (const event in blitzWarEvents) {
         events.push(blitzWarEvents[event]);
     }
 
-    // Heroic: Every Tuesday after a Raid, starting at 19:00 UTC, goes for 96 hours
     var heroicEvents = generateHeroicDates(startDate, endDate);
     for (const event in heroicEvents) {
         events.push(heroicEvents[event]);
@@ -70,183 +61,212 @@ function generateEvents(startDate, endDate) {
 
 function generateElementMonths(startDate, endDate) {
     const raidEvents = [];
-    console.log(startDate, endDate)
-//    if (startDate.getMonth() == endDate.getMonth() && startDate.getYear() == endDate.getYear()) {
-        const monthElementData = dateToElementMonth(startDate);
-        raidEvents.push(
-            {
-                title: monthElementData.name,
-                start: startDate,
-                end: endDate,
-                color: monthElementData.color,
-                display: 'background'
-            }
-        )
-        console.log(raidEvents);
-        return raidEvents;
-//    }
+    const monthElementData = dateToElementMonth(startDate);
+    raidEvents.push(
+        {
+            title: monthElementData.name,
+            start: startDate,
+            end: endDate,
+            color: monthElementData.color,
+            display: 'background'
+        }
+    )
+    return raidEvents;
 }
 
 function generateRaidDates(startDate, endDate) {
-    let raidDate = new Date("2024-01-12");
+    var raidEvents = [];
+    var possibleDate = new Date(startDate);
 
-    const raidEvents = [];
-
-    // Move into the correct date range
-    while (raidDate > startDate) {
-        raidDate.setDate(raidDate.getDate() - 14);
-    }
-    while (raidDate < startDate) {
-        raidDate.setDate(raidDate.getDate() + 14);
-    }
-
-    // Loop to generate every other Friday within the date range
-    while (raidDate >= startDate && raidDate <= endDate) {
-        raidEvents.push({
-            title: 'Raid',
-            start: new Date(raidDate.setUTCHours(19, 0, 0)),
-            end: new Date(addDays(raidDate, 2).setUTCHours(22, 0, 0)),
-            color: "#001219",
-            url: "../events/raid"
-        });
-        raidDate.setDate(raidDate.getDate() + 14);
+    while (possibleDate >= startDate && possibleDate <= endDate) {
+        if (!(possibleDate.getDay() == 5)) {
+            possibleDate = addDays(possibleDate, 1);
+            continue
+        }
+        if (isRaidDate(possibleDate)) {
+            raidEvents.push({
+                title: 'Raid',
+                start: new Date(possibleDate.setUTCHours(19, 0, 0)),
+                end: new Date(addDays(possibleDate, 2).setUTCHours(22, 0, 0)),
+                color: "#001219",
+                url: "../events/raid"
+            });
+        }
+        possibleDate = addDays(possibleDate, 1);
     }
 
     return raidEvents;
 }
 
+function isRaidDate(date) {
+    dateSeconds = date.getTime() / 1000;
+    dateMinutes = dateSeconds / 60;
+    dateHours = Math.floor(dateMinutes / 60);
+    dateDays = (dateHours - (dateHours % 24)) / 24;
+    dateDaysSinceFirstFriday = dateDays - 1;
+    dateDaysSinceFirstRaid = dateDaysSinceFirstFriday - 7;
+    return dateDaysSinceFirstRaid % 14 == 0;
+}
+
 function generateWarDates(startDate, endDate) {
-    let warDate = new Date("2024-01-05");
+    var warEvents = [];
+    var possibleDate = new Date(startDate);
 
-    const warEvents = [];
-
-    // Move into the correct date range
-    while (warDate > startDate) {
-        warDate.setDate(warDate.getDate() - 14);
-    }
-    while (warDate < startDate) {
-        warDate.setDate(warDate.getDate() + 14);
-    }
-
-    // Loop to generate every other Friday within the date range
-    while (warDate >= startDate && warDate <= endDate) {
-        warEvents.push({
-            title: 'War',
-            start: new Date(warDate.setUTCHours(19, 0, 0)),
-            end: new Date(addDays(warDate, 2).setUTCHours(22, 0, 0)),
-            color: "#9b2226",
-            url: "../events/war"
-        });
-        warDate.setDate(warDate.getDate() + 14);
+    while (possibleDate >= startDate && possibleDate <= endDate) {
+        if (!(possibleDate.getDay() == 5)) {
+            possibleDate = addDays(possibleDate, 1);
+            continue
+        }
+        if (isWarDate(possibleDate)) {
+            warEvents.push({
+                title: 'War',
+                start: new Date(possibleDate.setUTCHours(19, 0, 0)),
+                end: new Date(addDays(possibleDate, 2).setUTCHours(22, 0, 0)),
+                color: "#9b2226",
+                url: "../events/war"
+            });
+        }
+        possibleDate = addDays(possibleDate, 1);
     }
 
     return warEvents;
 }
 
+function isWarDate(date) {
+    dateSeconds = date.getTime() / 1000;
+    dateMinutes = dateSeconds / 60;
+    dateHours = Math.floor(dateMinutes / 60);
+    dateDays = (dateHours - (dateHours % 24)) / 24;
+    dateDaysSinceFirstFriday = dateDays - 1;
+    dateDaysSinceFirstWar = dateDaysSinceFirstFriday;
+    return dateDaysSinceFirstWar % 14 == 0;
+}
+
+
 function generateBlitzRaidDates(startDate, endDate) {
-    let blitzRaidDate = new Date("2024-01-08");
+    var blitzRaidEvents = [];
+    var possibleDate = new Date(startDate);
 
-    const blitzRaidEvents = [];
-
-    // Move into the correct date range
-    while (blitzRaidDate > startDate) {
-        blitzRaidDate.setDate(blitzRaidDate.getDate() - 14);
+    while (possibleDate >= startDate && possibleDate <= endDate) {
+        if (!(possibleDate.getDay() == 1)) {
+            possibleDate = addDays(possibleDate, 1);
+            continue
+        }
+        if (isBlitzRaidDate(possibleDate)) {
+            blitzRaidEvents.push({
+                title: 'Blitz Raid',
+                start: new Date(possibleDate.setUTCHours(19, 0, 0)),
+                end: new Date(addDays(possibleDate, 1).setUTCHours(22, 0, 0)),
+                color: "#0a9396",
+                url: "../events/blitz_raid"
+            });
+        }
+        possibleDate = addDays(possibleDate, 1);
     }
-    while (blitzRaidDate < startDate) {
-        blitzRaidDate.setDate(blitzRaidDate.getDate() + 14);
-    }
+    return blitzRaidEvents
+}
 
-    // Loop to generate every other Friday within the date range
-    while (blitzRaidDate >= startDate && blitzRaidDate <= endDate) {
-        blitzRaidEvents.push({
-            title: 'Blitz Raid',
-            start: new Date(blitzRaidDate.setUTCHours(19, 0, 0)),
-            end: new Date(addDays(blitzRaidDate, 1).setUTCHours(22, 0, 0)),
-            color: "#0a9396",
-            url: "../events/blitz_raid"
-        });
-        blitzRaidDate.setDate(blitzRaidDate.getDate() + 14);
-    }
-
-    return blitzRaidEvents;
+function isBlitzRaidDate(date) {
+    dateSeconds = date.getTime() / 1000;
+    dateMinutes = dateSeconds / 60;
+    dateHours = Math.floor(dateMinutes / 60);
+    dateDays = (dateHours - (dateHours % 24)) / 24;
+    dateDaysSinceFirstMonday = dateDays - 4;
+    dateDaysSinceFirstBlitzRaid = dateDaysSinceFirstMonday;
+    return dateDaysSinceFirstBlitzRaid % 14 == 0;
 }
 
 
 function generateBlitzWarDates(startDate, endDate) {
-    let blitzWarDate = new Date("2024-01-15");
+    var blitzWarEvents = [];
+    var possibleDate = new Date(startDate);
 
-    const blitzWarEvents = [];
-
-    // Move into the correct date range
-    while (blitzWarDate > startDate) {
-        blitzWarDate.setDate(blitzWarDate.getDate() - 14);
+    while (possibleDate >= startDate && possibleDate <= endDate) {
+        if (!(possibleDate.getDay() == 1)) {
+            possibleDate = addDays(possibleDate, 1);
+            continue
+        }
+        if (isSetBlitzDate(possibleDate)) {
+            blitzWarEvents.push({
+                title: 'Set Blitz',
+                start: new Date(possibleDate.setUTCHours(19, 0, 0)),
+                end: new Date(addDays(possibleDate, 1).setUTCHours(22, 0, 0)),
+                color: "#bb3e03",
+                borderColor: "#1F51FF",
+                url: "../events/set_blitz"
+            });
+            possibleDate = addDays(possibleDate, 1);
+        }
+        if (isBlitzWarDate(possibleDate)) {
+            blitzWarEvents.push({
+                title: 'Blitz War',
+                start: new Date(possibleDate.setUTCHours(19, 0, 0)),
+                end: new Date(addDays(possibleDate, 1).setUTCHours(22, 0, 0)),
+                color: "#bb3e03",
+                url: "../events/blitz_war"
+            });
+        }
+        possibleDate = addDays(possibleDate, 1);
     }
-    while (blitzWarDate < startDate) {
-        blitzWarDate.setDate(blitzWarDate.getDate() + 14);
-    }
-
-    // Loop to generate every other Friday within the date range
-    while (blitzWarDate >= startDate && blitzWarDate <= endDate) {
-        blitzWarEvents.push({
-            title: 'Blitz War',
-            start: new Date(blitzWarDate.setUTCHours(19, 0, 0)),
-            end: new Date(addDays(blitzWarDate, 1).setUTCHours(22, 0, 0)),
-            color: "#bb3e03",
-            url: "../events/blitz_war"
-        });
-        blitzWarDate.setDate(blitzWarDate.getDate() + 14);
-    }
-
-    return blitzWarEvents;
+    return blitzWarEvents
 }
+
+function isBlitzWarDate(date) {
+    dateSeconds = date.getTime() / 1000;
+    dateMinutes = dateSeconds / 60;
+    dateHours = Math.floor(dateMinutes / 60);
+    dateDays = (dateHours - (dateHours % 24)) / 24;
+    dateDaysSinceFirstMonday = dateDays - 4;
+    dateDaysSinceFirstBlitzWar = dateDaysSinceFirstMonday - 7;
+    return dateDaysSinceFirstBlitzWar % 14 == 0;
+}
+
+function isSetBlitzDate(date) {
+    if (!(isBlitzWarDate(date))) {
+        return false
+    }
+    return date.getDate() < 14
+}
+
 
 function generateHeroicDates(startDate, endDate) {
-    let heroicDate = new Date("2024-01-9");
+    var heroicEvents = [];
+    var possibleDate = new Date(startDate);
 
-    const heroicEvents = [];
-
-    // Move into the correct date range
-    while (heroicDate > startDate) {
-        heroicDate.setDate(heroicDate.getDate() - 14);
+    while (possibleDate >= startDate && possibleDate <= endDate) {
+        if (!(possibleDate.getDay() == 2)) {
+            possibleDate = addDays(possibleDate, 1);
+            continue
+        }
+        if (isHeroicDate(possibleDate)) {
+            heroicEvents.push({
+                title: 'Heroic',
+                start: new Date(possibleDate.setUTCHours(19, 0, 0)),
+                end: new Date(addDays(possibleDate, 7).setUTCHours(19, 0, 0)),
+                color: "#e9c46a",
+                url: "../events/heroic"
+            });
+        }
+        possibleDate = addDays(possibleDate, 1);
     }
-    while (heroicDate < startDate) {
-        heroicDate.setDate(heroicDate.getDate() + 14);
-    }
-
-    // Loop to generate every other Friday within the date range
-    while (heroicDate >= startDate && heroicDate <= endDate) {
-        heroicEvents.push({
-            title: 'Heroic',
-            start: new Date(heroicDate.setUTCHours(19, 0, 0)),
-            end: new Date(addDays(heroicDate, 7).setUTCHours(19, 0, 0)),
-            color: "#e9c46a",
-            url: "../events/heroic"
-        });
-        heroicDate.setDate(heroicDate.getDate() + 14);
-    }
-
-    return heroicEvents;
+    return heroicEvents
 }
+
+function isHeroicDate(date) {
+    dateSeconds = date.getTime() / 1000;
+    dateMinutes = dateSeconds / 60;
+    dateHours = Math.floor(dateMinutes / 60);
+    dateDays = (dateHours - (dateHours % 24)) / 24;
+    dateDaysSinceFirstTuesday = dateDays - 5;
+    dateDaysSinceFirstHeroic = dateDaysSinceFirstTuesday;
+    return dateDaysSinceFirstHeroic % 14 == 0;
+}
+
 
 function addDays(date, days) {
   var newDate = new Date(date);
   newDate.setDate(date.getDate() + days);
   return newDate;
-}
-
-function isElement(obj) {
-  try {
-    //Using W3 DOM2 (works for FF, Opera and Chrome)
-    return obj instanceof HTMLElement;
-  }
-  catch(e){
-    //Browsers not supporting W3 DOM2 don't have HTMLElement and
-    //an exception is thrown and we end up here. Testing some
-    //properties that all elements have (works on IE7)
-    return (typeof obj==="object") &&
-      (obj.nodeType===1) && (typeof obj.style === "object") &&
-      (typeof obj.ownerDocument ==="object");
-  }
 }
 
 function dateToElementMonth(date) {
